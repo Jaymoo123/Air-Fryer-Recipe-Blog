076610f27a066958bd64594815db05329d847b04
Jaymoo123 <jeff@emplifex.com>
2025-08-03 20:44:41 +0100

    Add files via upload
    Test

---
+++ src/content/config.ts
@@ -0,0 +1,22 @@
+import { defineCollection, z } from "astro:content";
+
+const blog = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    slug: z.string(),
+    date: z.string(),
+    author: z.string(),
+    category: z.string(),
+    metaTitle: z.string(),
+    metaDescription: z.string(),
+    altText: z.string(),
+    image: z.string(),
+    h1: z.string(),
+    summary: z.string(),
+    schema: z.string()
+  })
+});
+
+export const collections = {
+  blog,
+};
